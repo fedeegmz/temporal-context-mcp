5f9f3aea67f9f5bb39f51672808c8e4947ddf2f2
Federico Gomez <gomez00federico@gmail.com>
2025-08-05 20:03:19 -0300

    feat: add id utils

--- src/temporal_context_mcp/shared/__init__.py
+++ src/temporal_context_mcp/shared/__init__.py
@@ -2,6 +2,7 @@ from temporal_context_mcp.shared.application.time_pattern_utils import TimePatte
 from temporal_context_mcp.shared.domain.time_pattern import TimePattern
 from temporal_context_mcp.shared.domain.utils.datetime_utils import get_current_datetime
 from temporal_context_mcp.shared.domain.utils.decorators import default_false
+from temporal_context_mcp.shared.domain.utils.id_utils import generate_id
 from temporal_context_mcp.shared.domain.utils.json_utils import (
     load_models_from_json_file,
     save_models_to_json_file,
@@ -15,6 +16,7 @@ __all__ = [
     "TimePattern",
     "TimePatternUtils",
     "default_false",
+    "generate_id",
     "get_current_datetime",
     "load_models_from_json_file",
     "save_models_to_json_file",
